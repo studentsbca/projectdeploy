c3bdc46f1f78708f4adf1ba68f1095e4afe9e1ee
studentsbca <sahanianuj7860@gmail.com>
2024-08-10 07:37:10 +0530

    Add files via upload

---
+++ assets/food.js/food.js
@@ -0,0 +1,19 @@
+
+function fun(){
+    alert("sorry for not avilable");
+   }
+   
+   function openwindow1(){
+    window.open("https://www.linkedin.com");
+   }
+   function openwindow2(){
+    window.open("https://www.youtube.com");
+   }
+   function openwindow3(){
+    window.open("https://www.instagram.com");
+   }
+   function openwindow4(){
+    window.open("https://www.twitter.com");
+   }
+
+   
